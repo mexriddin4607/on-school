001aba6edff0f44651ae553a54e06124c798336f
mexriddin4607 <mexriddinavmedov63@gmail.com>
2025-01-08 20:48:31 +0500

    bajarildi

--- school/students.py
+++ school/students.py
@@ -1,41 +1,48 @@
-def register_student(students_data: dict[str, dict[str, str]]) -> None:
-    """
-    Registers a new student by collecting their name, email, and password, 
-    and stores the information in the students_data dictionary.
+def register_student(student : dict[str, dict[str, str]]) -> None:
+    
+    student['name'] = input("Enter your name: ")
 
-    Args:
-        students_data (dict): A dictionary where student emails are keys 
-                               and their details (name and password) are stored as values.
-    """
-    pass
+    student['course'] = []
 
-def login_student(students_data: dict[str, dict[str, str]]) -> str | None:
-    """
-    Allows a student to log in by entering their email and password. 
-    If the login is successful, it returns the student's name.
+    student_email = input("Enter your email: ")
+    student_password = input("Enter your password: ")
+    
+    
+    student['pas_log'] = {}
+    student['pas_log']['log'] = student_email
+   
+    return student
 
-    Args:
-        students_data (dict): A dictionary where student emails are keys 
-                               and their details (name and password) are stored as values.
+def login_student(students_data: list) -> str | None:
+   
+    
+    login = input("Enter your email: ")
+    passwod = input("Enter your password: ")
 
-    Returns:
-        str: The student's name if login is successful, else None.
-    """
-    pass
+    student_dict = {}
+    student_dict['log'] = login
+    student_dict['pass'] = passwod
+    
+    
+    for user in students_data:
+        if(user['pas_log']) == student_dict:
+            print("Login successful! Welcome back, {}.".format(user['name']))
+            return user
+    return None
+
 
 def enroll_in_course(
     courses_data: list[dict[str, str]], 
     students_data: dict[str, dict[str, list[str]]], 
     student_email: str
 ) -> None:
-    """
-    Allows a student to enroll in a course by selecting from the available courses. 
-    The selected course is added to the student's list of enrolled courses.
 
-    Args:
-        courses_data (list): A list of dictionaries containing available course details.
-        students_data (dict): A dictionary where student emails are keys 
-                               and their details (including enrolled courses) are stored as values.
-        student_email (str): The email of the student who is enrolling.
-    """
-    pass
+    courses_number = int(input("Select a course number to enroll: "))
+
+    if courses_number - 1 > len(courses_data) or courses_number - 1 < 0:
+        print("\nSiz tanlagan nommirda kurs yo`q iltimos tikshirib qaytadan tanlang!\n")
+        return None
+    else:
+        print("Successfully enrolled in {}!\n".format(courses_data[courses_number - 1]["course_name"]))
+        return courses_data[courses_number - 1]
+   
